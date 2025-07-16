f6486bb1d364f53aad941d8a867a64838d9e9a16
bahdman <anetoroselumese@gmail.com>
2025-07-16 10:05:48 +0100

    add: initiatilize result module

---
+++ src/Interface/IResultRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace src.Interface
+{
+    public interface IResultRepository
+    {
+        Task<string> CreateResult();
+        Task<string> ViewResult();
+        Task<string> ViewAllResult();
+    }
+}
